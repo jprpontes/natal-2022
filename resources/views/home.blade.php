e4b10b5b6bf76700622606401e4e1e313bdce635
João Paulo <joaopauloribeiropontes@gmail.com>
2022-11-12 23:10:06 -0300

    Habilitando timer.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -305,7 +305,7 @@
         </div>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-        {{-- <script>
+        <script>
             // Set the date we're counting down to
             var countDownDate = new Date("Dec 23, 2022 19:00:00").getTime();
 
@@ -336,6 +336,6 @@
                 document.getElementById("timer").innerHTML = "EXPIRED";
               }
             }, 1000);
-        </script> --}}
+        </script>
     </body>
 </html>
